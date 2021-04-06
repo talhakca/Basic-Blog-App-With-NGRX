390185ac8e08607dbdc0d08a33183dd686e0fb23
talha <talhakca@gmail.com>
2021-04-07 01:55:17 +0300

    server.js typo fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/angularappindex.html'));
+    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
